09df270525d114924d818a88bb0843b0a49c07fa
Nate Sawant <natesawant@gmail.com>
2024-10-01 12:10:00 -0400

    Initialize Astro project with tailwind

--- app/components/sections/experience.tsx
+++
@@ -1,6 +0,0 @@
-export default function Experience() {
-    return <div className=" flex flex-col gap-2">
-        <p>While completing my bachelors, I have done a two 6-month co-ops at two different companies, a large financial company and a medium size text analytics company.</p>
-        <p>{"At Northeastern University, I also had the opportunity to work in a research lab and work on various projects Northeastern's student-led product development studio."}</p>
-    </div>
-}
