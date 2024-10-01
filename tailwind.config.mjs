6220b0ce5474404a2b8ed4a91e7ac4ae70abd325
Nate Sawant <natesawant@gmail.com>
2024-10-01 14:47:35 -0400

    No custom font yet

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'long-exposure': "url('/img/longexposure.jpg')",
+				'skyline': "url('/img/skyline.png')",
+				'franconia': "url('/img/franconia.png')",
+			},
+		},
 	},
 	plugins: [],
 }
